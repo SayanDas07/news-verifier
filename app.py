66f4c5cab24141f9704d36b413754d0a9b3e338e
Sayan Das <sayandasraja56@gmail.com>
2025-01-26 16:53:41 +0530

    update CORS configuration to allow requests from production domain

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["https://news-verifier-kappa.vercel.app/"])
 
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
